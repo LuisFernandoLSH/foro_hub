8f93fee632e01509cd30358f013a82c8e28c0e41
LuisFernandoLSH <luisgarcia3102000@gmail.com>
2024-07-14 18:02:32 -0600

    Add configuration

---
+++ src/main/java/com/alura/lsh/foroHub/domain/topico/Topico.java
@@ -0,0 +1,9 @@
+package com.alura.lsh.foroHub.domain.topico;
+
+public class Topico {
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private String fechaCreacion;
+    private Boolean status;
+}
